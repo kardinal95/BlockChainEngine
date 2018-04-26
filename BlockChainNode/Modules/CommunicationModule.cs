dc6ae853cfe04c5c94aa50d0ae2463fa6e9aba25
Patapata3 <32408943+Patapata3@users.noreply.github.com>
2018-04-26 21:01:52 +0500

    Update CommunicationModule.cs

--- BlockChainNode/Modules/CommunicationModule.cs
+++ BlockChainNode/Modules/CommunicationModule.cs
@@ -1,6 +1,7 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Configuration;
 using BlockChainNode.Net;
+using BlockChainNode.Lib.Logging;
 using Nancy;
 using Nancy.Extensions;
 using Nancy.Responses;
@@ -22,8 +23,10 @@ namespace BlockChainNode.Modules
 
         public JsonResponse Sync()
         {
+            Logger.Log.Info($"Запрос на {this.Request.Url} от {this.Request.UserHostAddress}" +
+                            $"на синхронизацию узлов...");
             NodeBalance.PerformOnAllNodes(NodeBalance.RebalanceNode);
-
+            Logger.Log.Debug($"Возвращено число узлов: {NodeBalance.NodeSet.Count}");
             return new JsonResponse(
                 new Dictionary<string, string>
                 {
@@ -31,8 +34,12 @@ namespace BlockChainNode.Modules
                 }, new JsonNetSerializer()) {StatusCode = HttpStatusCode.OK};
         }
 
-        private static JsonResponse GetNodeInfo()
+        private JsonResponse GetNodeInfo()
         {
+            Logger.Log.Info($"Запрос на {this.Request.Url} от {this.Request.UserHostAddress} " +
+                            $"на получение информации об узлах");
+            Logger.Log.Debug($"Адрес хоста: {ConfigurationManager.AppSettings["host"]}\n" +
+                             $"Количество узлов: {NodeBalance.NodeSet.Count}");
             return new JsonResponse(
                 new Dictionary<string, string>
                 {
@@ -46,9 +53,12 @@ namespace BlockChainNode.Modules
             var jsonString = Request.Body.AsString();
             var jsonObject = JObject.Parse(jsonString);
             var host = (string) jsonObject["host"];
+            Logger.Log.Info($"Запрос на {this.Request.Url} от {this.Request.UserHostAddress} " +
+                            $"на регистрацию нового узла");
 
             if (string.IsNullOrEmpty(host))
             {
+                Logger.Log.Error("Не предоставлены данные о хосте");
                 return new JsonResponse(null, new JsonNetSerializer())
                 {
                     StatusCode = HttpStatusCode.BadRequest
@@ -56,6 +66,9 @@ namespace BlockChainNode.Modules
             }
 
             NodeBalance.NodeSet.Add(host);
+            Logger.Log.Info("Добавлен новый хост");
+            Logger.Log.Debug($"Адрес хоста: {host}");
+            Logger.Log.Debug($"Количество узлов: {NodeBalance.NodeSet.Count}");
             return new JsonResponse(NodeBalance.NodeSet, new JsonNetSerializer())
             {
                 StatusCode = HttpStatusCode.OK
